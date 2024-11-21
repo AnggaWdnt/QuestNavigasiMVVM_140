ac872b0930a8d7d98cd40277f002620c2bab0617
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-11-21 23:58:08 +0700

    tambahan value email,notelepon, nim di DataSiswa

--- app/src/main/java/com/example/praktikum7/model/DataSiswa.kt
+++ app/src/main/java/com/example/praktikum7/model/DataSiswa.kt
@@ -2,4 +2,7 @@ data class DataSiswa(
     val nama: String = "",
     val gender: String = "",
     val alamat: String = "",
+    val email: String = "",
+    val notelepon: String = "",
+    val nim:String=""
 )
